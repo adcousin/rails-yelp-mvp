8543be4750c895eb857232860bf89c07e761904e
adrien cousin <adrien.cousin@lilo.org>
2020-11-18 16:00:53 +0100

    Implemented reviews controller with new and create

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   # root to: 'restaurants#index', as: 'restaurants'
+  get 'reviews/create'
   get '/restaurants', to: 'restaurants#index', as: 'restaurants'
   get 'restaurants/new', to: 'restaurants#new', as: 'new_restaurant'
   post 'restaurants', to: 'restaurants#create'
-  # get 'restaurants/create'
-  get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant'
-  get 'restaurants/:id/reviews/new', to: 'reviews#new', as: 'new_restaurant_review'
+
+  # get 'restaurants/:id', to: 'restaurants#show', as: 'restaurant' do
+  #   # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: 'new_restaurant_review'
+  #   get 'reviews/new', to: 'reviews#new', as: 'new_restaurant_review'
+  # end
+  resources :restaurants, only: [:show] do
+    resources :reviews, only: [:new, :create]
+  end
+
 end
